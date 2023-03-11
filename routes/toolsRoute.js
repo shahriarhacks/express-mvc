faed7249b5754aea1c57a39fe13acee6c5e293fb
SHAHRIAR AHMED <shahriar7ahmed@gmail.com>
2023-03-11 16:04:25 +0600

    Changed

--- routes/toolsRoute.js
+++ routes/toolsRoute.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Tools Found");
-});
-router.post("/", (req, res) => {
-  res.send("tools created success");
-});
+// router.get("/", (req, res) => {
+//   res.send("Tools Found");
+// });
+// router.post("/", (req, res) => {
+//   res.send("tools created success");
+// });
+
+router
+  .route("/")
+  .get((req, res) => {
+    res.send("Tools Found");
+  })
+  .post((req, res) => {
+    res.send("tools created success");
+  });
 
 module.exports = router;
